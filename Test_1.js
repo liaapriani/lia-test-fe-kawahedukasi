477709fe711937aa8a31b9262e2957b11cba90f5
liaapriani <109080998+liaapriani@users.noreply.github.com>
2022-07-13 15:17:51 +0700

    Update Test_1.js

--- Test_1.js
+++ Test_1.js
@@ -1,26 +1,25 @@
-let data1 = [1, 1, 1, 2, 2, 4, 1, 1]
-let data2 = [2, 1, 2, 2, 2, 8, 1, 1]
-let data3 = [3, 3, 3, 3, 2, 4, 1, 1]
-
+let data1 = [1, 1, 1, 2, 2, 4, 1, 1];
+let data2 = [2, 1, 2, 2, 2, 8, 1, 1];
+let data3 = [3, 3, 3, 3, 2, 4, 1, 1];
 const mostFrequent = (arr) => {
-    let max = 0;
-    let maxIndex = 0;
-    for (let i = 0; i < arr.length; i++) {
-        let count = 0;
-        for (let j = 0; j < arr.length; j++) {
-            if (arr[i] === arr[j]) {
-                count++;
-            }
-        }
-        if (count > max) {
-            max = count;
-            maxIndex = i;
-        }
+  let max = 0;
+  let maxIndex = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let count = 0;
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        count++;
+      }
     }
-    return arr[maxIndex];
+    if (count > max) {
+      max = count;
+      maxIndex = i;
+    }
+  }
+  return arr[maxIndex];
 }
 const countNumber = (arr, num) => arr.filter((item) => item === num).length;
 
 [data1, data2, data3].forEach((arr) => {
-    console.log('Total data paling banyak keluar adalah ${mostFrequent(arr)} dengan jumlah ${countNumber(arr, mostFrequent(arr))}')
+  console.log(`total data paling banyak keluar adalah ${mostFrequent(arr)} dengan jumlah ${countNumber(arr, mostFrequent(arr))}`)
 })
