44fa1ab70d18fb012733eddee94707ebc1c07e97
liaapriani <109080998+liaapriani@users.noreply.github.com>
2022-07-13 15:21:08 +0700

    Update Test_3.js

--- Test_3.js
+++ Test_3.js
@@ -17,22 +17,17 @@ console.log(segitiga1(6));
 
 //segitiga 2
 
-function segitiga2(batis){
-    let pola = '';
-
-    for(let i = 1; i <= baris; i++){
-        for(let j = baris - 1; j >= i; j++){
-            pola += ' ';
-        }
-
-        for(let k = 1; k <= i; k++){
-            pola += '*';
-        }
-
-        pola += '\n;'
+const rightTriangle = (n) => {
+  let string = "";
+  for (let i = 1; i <= n; i++) {
+    for (let j = 0; j < n - i; j++) {
+      string += " ";
     }
-
-    return pola;
-}
-
-console.log(segitiga2(6));
+    for (let k = 0; k < i; k++) {
+      string += "*";
+    }
+    console.log(string);
+    string = "";
+  }
+};
+rightTriangle(5);
